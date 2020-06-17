e01221facd476cdf41da5c61e97f28c16cf2c006
Mateus Caetano <mateus.caetano@alu.ufc.br>
2020-06-16 21:00:14 -0300

    liked jobs screen

--- src/screens/likedJobs/index.js
+++ src/screens/likedJobs/index.js
@@ -1,14 +1,77 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { connect } from 'react-redux';
-import { Card, Button, Title } from 'react-native-paper';
+import { Card, Button, Title, Text } from 'react-native-paper';
 import MapView, { Marker } from 'react-native-maps';
 
 function LikedJobs(props) {
   const navigation = useNavigation();
 
-  return <View></View>;
+  return (
+    <ScrollView
+      style={{
+        marginTop: StatusBar.currentHeight + 15,
+        marginHorizontal: 30,
+      }}
+      showsVerticalScrollIndicator={false}
+    >
+      {props.likedJobs.map((job, index) => (
+        <Card
+          style={{
+            paddingBottom: 15,
+            borderWidth: 1,
+            marginBottom: 20,
+          }}
+        >
+          <Card.Title title={`${job.job} ${index} `} />
+          <Card.Content style={{ height: 200 }}>
+            <MapView
+              style={{ flex: 1 }}
+              scrollEnabled={false}
+              initialRegion={{
+                ...job.location,
+                latitudeDelta: 0.045,
+                longitudeDelta: 0.02,
+              }}
+            >
+              <Marker
+                title={job.job}
+                coordinate={{
+                  ...job.location,
+                  latitudeDelta: 0.5,
+                  longitudeDelta: 0.5,
+                }}
+              />
+            </MapView>
+          </Card.Content>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              paddingTop: 15,
+            }}
+          >
+            <Title>{job.company}</Title>
+            <Title>{job.relativeTime}</Title>
+          </View>
+          <Button
+            style={{
+              marginHorizontal: 30,
+              backgroundColor: '#009688',
+              marginTop: 10,
+              elevation: 10,
+            }}
+            onPress={() => {}}
+            mode="contained"
+          >
+            <Title style={{ color: '#fff' }}>Apply</Title>
+          </Button>
+          <Text style={{ padding: 15 }}>{job.snipet}</Text>
+        </Card>
+      ))}
+    </ScrollView>
+  );
 }
 
 function mapStateToProps(state) {
